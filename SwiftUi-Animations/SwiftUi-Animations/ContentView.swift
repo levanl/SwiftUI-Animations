ab91dda1cfd2becc83c11ef3f01ad5cd941747e1
Levan Loladze <levan.loladzew@gmail.com>
2023-12-23 18:10:16 +0400

    first commit

---
+++ SwiftUi-Animations/SwiftUi-Animations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUi-Animations
+//
+//  Created by Levan Loladze on 23.12.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUi-Animations/SwiftUi-Animations/SwiftUi_AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUi_AnimationsApp.swift
+//  SwiftUi-Animations
+//
+//  Created by Levan Loladze on 23.12.23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUi_AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
